302d8378b3bbd1a5b89ca3d1a3929cda9696d5ee
takahigu642 <takahigu642@gmail.com>
2023-08-03 21:19:40 +0900

    modify cacl.js

--- calc.js
+++ calc.js
@@ -1,13 +1,13 @@
-let num = Math.floor(Math.random() * 50);
+let num = 45;
 
-if(num % 3 === 0){
-    console.log("3の倍数です");
+if(num % 15 === 0){
+    console.log("3と5の倍数です");
 }
 else if(num % 5 === 0){
     console.log("5の倍数です");
 } 
-else if(num % 15 === 0){
-    console.log("3と5の倍数です");
+else if(num % 3 === 0){
+    console.log("3の倍数です");
 }
 else{
     console.log(num);
